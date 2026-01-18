6632485f93f4b27f161d406d5f78f83bc4ccb477
Hrushikesh <sannidhanamhrushikesh@gmail.com>
2026-01-18 15:27:50 +0530

    nuqs adapter issue resolved

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -1,38 +1,59 @@
 "use client"
+
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
 
 import { Sidebar } from "./sidebar";
 import { Toolbar } from "./toolbar";
 import { WorkspaceSidebar } from "./WorkspaceSidebar";
-interface WorkspaceIdLayoutProps{
+import { usePanel } from "@/hooks/use-panel";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
+
+interface WorkspaceIdLayoutProps {
     children: React.ReactNode
 };
 
-const WorkspaceIdLayout = ({children}:WorkspaceIdLayoutProps)=>{
-    return(
+const WorkspaceIdLayout = ({ children }: WorkspaceIdLayoutProps) => {
+
+    return (
+        <NuqsAdapter>
+            <InnerLayout>{children}</InnerLayout>
+        </NuqsAdapter>
+    );
+};
+
+const InnerLayout = ({ children }: WorkspaceIdLayoutProps) => {
+    const { parentMessageId, onClose } = usePanel();
+    const showPanel = !!parentMessageId;
+
+    return (
         <div className="h-full">
-            <Toolbar/>
-            <div className="flex h-[calc(100vh-40px)]"> 
-                <Sidebar/>
+            <Toolbar />
+            <div className="flex h-[calc(100vh-40px)]">
+                <Sidebar />
                 <ResizablePanelGroup direction="horizontal" autoSaveId="ca-workspace-layout">
                     <ResizablePanel
                         defaultSize={20}
                         minSize={11}
                         className="bg-[#5E2C5F]"
                     >
-                        <WorkspaceSidebar/>
+                        <WorkspaceSidebar />
                     </ResizablePanel>
-                    <ResizableHandle withHandle/>
+                    <ResizableHandle withHandle />
                     <ResizablePanel minSize={20}>
                         {children}
                     </ResizablePanel>
+                    {showPanel && (
+                        <>
+                            <ResizableHandle withHandle />
+                            <ResizablePanel minSize={20} defaultSize={29}>
+                                Load Thread
+                            </ResizablePanel>
+                        </>
+                    )}
                 </ResizablePanelGroup>
-
-                
             </div>
-            
         </div>
-    )
-}
-export default WorkspaceIdLayout;
+    );
+};
 
+export default WorkspaceIdLayout;
